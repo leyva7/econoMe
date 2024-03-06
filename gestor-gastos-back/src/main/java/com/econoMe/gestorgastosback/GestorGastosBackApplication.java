99847715a507308af7a8f2f746654694ac79ecf5
Jesus Leyva <jesusleyvafer@gmail.com>
2024-03-06 19:18:52 +0100

    Primer commit

---
+++ gestor-gastos-back/src/main/java/com/econoMe/gestorgastosback/GestorGastosBackApplication.java
@@ -0,0 +1,13 @@
+package com.econoMe.gestorgastosback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorGastosBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestorGastosBackApplication.class, args);
+	}
+
+}
---
+++ gestor-gastos-back/src/test/java/com/econoMe/gestorgastosback/GestorGastosBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.econoMe.gestorgastosback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorGastosBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
